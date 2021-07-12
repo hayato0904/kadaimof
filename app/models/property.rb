2e77526fd1fe340012e61d9ca2b148dccb8b33ec
hayato0904 <sakigi94@gmail.com>
2021-07-12 12:52:39 +0000

    入力された値が空の場合エラーを発生させる。

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,17 @@
 class Property < ApplicationRecord
+    validates_presence_of :property_name,
+          message: "物件名を入れてください！"
+
+          validates_presence_of :address,
+          message: "住所を入れてください！"
+
+          validates_presence_of :age,
+          message: "築年数を入力してください！"
+
+          validates_presence_of :note,
+          message: "備考欄"
+
+      
     # アソシエーション設定されている。
     has_many :nearest_stations, dependent: :destroy
     # trueが出た時は送信データから除外させていく。
